8d5eaaf6f6b0cf0900eaf927f9f020140d35e877
Valdemar Berenguer <jvaldemar11@hotmail.com>
2020-01-31 18:51:00 +0000

    Página Principal e Portfólio Feitos

--- src/Controller/PoolPrimeController.php
+++ src/Controller/PoolPrimeController.php
@@ -6,14 +6,49 @@
     use Symfony\Component\Routing\Annotation\Route;
     use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
     use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+    use Symfony\Component\HttpFoundation\Session\Session;
 
     class PoolPrimeController extends Controller{
         /**
-         * @Route("/")
+         * @Route("/", name="main")
+         */
+        public function main(){
+            $session = new Session();
+            if($session->get('language') == null){
+                $session->set('language', 'pt');
+            }
+            return $this->redirectToRoute('index',[
+                '_locale' => $session->get('language')
+            ]);
+        }
+
+        /**
+         * @Route("/{_locale}", name="index")
          */
         public function index(){
-            return $this->render('base.html.twig',[
-                
+            $session = new Session();
+            $route = $this->get('router')->getContext()->getPathInfo();
+            if($route == '/pt'){
+                $session->set('language', 'pt');
+            }else if($route == '/en'){
+                $session->set('language', 'en');
+            }else{
+                return $this->redirectToRoute('main');
+            }
+            $language = $session->get('language');
+            return $this->render('index.html.twig', [
+                'language' => $language
+            ]);
+        }
+
+        /**
+         * @Route("/portfolio/{_locale}", name="portfolio")
+         */
+        public function portfolio(){
+            $session = new Session();
+            $language = $session->get('language');
+            return $this->render('portfolio.html.twig', [
+                'language' => $language
             ]);
         }
     }
